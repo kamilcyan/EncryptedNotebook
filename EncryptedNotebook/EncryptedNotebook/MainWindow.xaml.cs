e9438383674200a474252f039f55079ca8a8d53e
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-01 21:25:24 +0100

    start

---
+++ EncryptedNotebook/EncryptedNotebook/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptedNotebook
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Notes> notes { get; set; }
+
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+
+            notes = new List<Notes>();
+            dataGrid.ItemsSource = notes;
+        }
+
+
+    }
+}
---
+++ EncryptedNotebook/EncryptedNotebook/Notes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptedNotebook
+{
+    public class Notes
+    {
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+        public string author { get; set; }
+    }
+}
